cfff49193454e3856127f0f9bcccd6d8b6fb855c
Jonatan C. Luz <dev.jonatancl@gmail.com>
2024-11-28 16:03:53 -0300

    Update Financeiro.py

--- Financeiro.py
+++ Financeiro.py
@@ -4,9 +4,12 @@ import pandas as pd
 from datetime import datetime
 import locale
 
-def run():
+try:
+    locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+except locale.Error:
+    locale.setlocale(locale.LC_ALL, 'C.UTF-8')
 
-    locale.setlocale(locale.LC_ALL, 'portuguese')
+def run():
 
     def conectar_db():
         return sqlite3.connect('banco.db')
