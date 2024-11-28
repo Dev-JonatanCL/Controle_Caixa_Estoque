9272814fbfe40b92f1a1e25c19bd98c922fc4d88
Jonatan C. Luz <dev.jonatancl@gmail.com>
2024-11-28 16:17:51 -0300

    Update Caixa.py

--- Caixa.py
+++ Caixa.py
@@ -28,7 +28,10 @@ def run():
             return data_str
 
     def formatar_contabil(valor):
-        return locale.currency(valor, grouping=True)
+        try:
+            return locale.currency(valor, grouping=True)
+        except ValueError:
+            return f"R$ {valor:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".")
 
     def listar_venda():
         conn = conectar_db()
