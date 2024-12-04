a6e428dc38fbb5b3acee5a76e7cbdb434b2b3f50
Dev-JonatamCL <dev.jonatancl@gmail.com>
2024-12-04 15:29:26 -0300

    Estilizando

--- Cadastro.py
+++ Cadastro.py
@@ -6,7 +6,6 @@ from datetime import datetime
 
 def run():
 
-
     def conectar_db():
         return sqlite3.connect('banco.db')
 
--- app.py
+++ app.py
@@ -2,6 +2,23 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 import importlib
 
+background_image_url = "https://img.freepik.com/vetores-gratis/fundo-branco-com-hexagono-de-tecnologia-azul_1017-19366.jpg"
+
+st.markdown(f"""
+    <style>
+        .st-emotion-cache-1r4qj8v {{
+            background-image: url('{background_image_url}');
+            background-size: cover;
+        }}
+        .stMainBlockContainer {{
+            background-color: #cccccc;
+        }}
+        .st-emotion-cache-6qob1r {{
+            background-color: white;
+        }}
+    </style>
+""", unsafe_allow_html=True)
+
 pages = {
     "Caixa": "Caixa",
     "Estoque": "Estoque",
@@ -14,9 +31,16 @@ with st.sidebar:
     pagina_selecionada = option_menu(
         menu_title="Selecione uma página",
         options=list(pages.keys()),
-        icons=["house", "box", "person", "credit-card", "bar-chart"],
+        icons=["cart4", "box", "person", "coin", "bar-chart"],
         menu_icon="cast",
-        default_index=0
+        default_index=0,
+        styles={
+            "menu-title": {"color": "#000", "font-size": "25px", "font-weight": "bold"},
+            "container": {"padding": "5px", "background-color": "#cccccc", "height": "465px"},
+            "icon": {"color": "blue", "font-size": "30px"},
+            "nav-link": {"font-size": "25px", "text-align": "left", "margin": "0px", "padding": "10px", "color": "#000"},
+            "nav-link-selected": {"background-color": "white", "color": "#000"},
+        }
     )
 
 module_name = pages[pagina_selecionada]
